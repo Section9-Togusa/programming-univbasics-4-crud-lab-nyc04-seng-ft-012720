a9885d988881addb941d1506453805781988f0cf
Daryl Corbin <corbindac@gmail.com>
2020-01-08 05:45:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 array = ["Dwight", "Micheal", "Jim", "Ryan"]
-element = ["Dwight", "Micheal", "Jim", "Ryan"]
+element = array << "Darryl"
 add_element_to_end_of_array(array, element)
 end
 
