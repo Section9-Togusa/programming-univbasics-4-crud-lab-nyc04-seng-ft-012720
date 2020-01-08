a8da70be8db645445ee48c51eb06437cf86a69a2
Daryl Corbin <corbindac@gmail.com>
2020-01-08 05:38:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-the_office = ["Dwight", "Micheal", "Jim",, "Ryan"]
+the_office = ["Dwight", "Micheal", "Jim", "Ryan"]
 end
 
 def add_element_to_end_of_array(array, element)
