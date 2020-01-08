5fb1028d997813b1f80aec6aeea7944fa99e8c1d
Daryl Corbin <corbindac@gmail.com>
2020-01-08 05:51:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,7 @@ def add_element_to_end_of_array(array, element)
 array = ["Dwight", "Micheal", "Jim", "Ryan"]
 element = array.push("Andy")
 end
+add_element_to_start_of_array
 
 
 def add_element_to_start_of_array(array, element)
